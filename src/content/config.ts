05d808c0011e3021a3a82e3f13e9f171c1dfbb3e
Marcus Mofjeld <au717932@uni.au.dk>
2025-07-23 17:43:29 +0200

    fixed the fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,8 +3,9 @@ import { cldAssetsLoader } from 'astro-cloudinary/loaders';
 
 const photographyCollection = defineCollection({
   loader: cldAssetsLoader({
-    folder: 'Portfolio', 
-  })
+    folder: 'Portfolio',
+    resourceType: 'image'
+    })
 });
 
 export const collections = {
